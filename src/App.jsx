a3c482eb8f7e6721a4e0028d68a18659ecaf6723
Mohammad Javad Alaeddini <mohammadjavada78@gmail.com>
2021-08-19 15:49:18 +0430

    build a component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Todo from "./component/Todo";
+const initialTodos = [
+  {
+    id: uuid(),
+    title: "Clean The House",
+    isCompleted: false,
+  },
+  {
+    id: uuid(),
+    title: "Buy A New Car",
+    isCompleted: false,
+  },
+  {
+    id: uuid(),
+    title: "Buy A New Laptop",
+    isCompleted: false,
+  },
+];
 const App = () => {
+  const [todos, setTodos] = useState(initialTodos);
   return (
-    <div class="page-content page-container" id="page-content">
-      <div class="row container d-flex justify-content-center">
-        <div class="col-md-12">
-          <div class="card px-3">
-            <div class="card-body">
-              <h4 class="card-title">Awesome Todo list</h4>
-              <div class="add-items d-flex">
+    <div className="page-content page-container" id="page-content">
+      <div className="row container d-flex justify-content-center">
+        <div className="col-md-12">
+          <div className="card px-3">
+            <div className="card-body">
+              <h4 className="card-title">Awesome Todo list</h4>
+              <div className="add-items d-flex">
                 <input
                   type="text"
-                  class="form-control todo-list-input"
+                  className="form-control todo-list-input"
                   placeholder="What do you need to do today?"
                 />
-                <button class="add btn btn-primary font-weight-bold todo-list-add-btn">
+                <button className="add btn btn-primary font-weight-bold todo-list-add-btn">
                   Add
                 </button>
               </div>
-              <div class="list-wrapper">
-                <ul class="d-flex flex-column todo-list">
-                  <li>
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input class="checkbox" type="checkbox" />
-                        Buy Milk.
-                        <i class="input-helper"> </i>
-                      </label>
-                    </div>
-                    <i class="remove mdi mdi-close-circle-outline"></i>
-                  </li>
-                  <li class="completed">
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        {" "}
-                        <input
-                          class="checkbox"
-                          type="checkbox"
-                          checked=""
-                        />{" "}
-                        Wash dishes.
-                        <i class="input-helper"></i>
-                      </label>
-                    </div>{" "}
-                    <i class="remove mdi mdi-close-circle-outline"></i>
-                  </li>
+              <div className="list-wrapper">
+                <ul className="d-flex flex-column todo-list">
+                  {todos.map((todo) => (
+                    <Todo todo={todo} />
+                  ))}
                 </ul>
               </div>
             </div>
